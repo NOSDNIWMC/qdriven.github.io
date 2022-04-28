42cad6564202ecc48ea7207beb264299a66f0fd4
Patrick <patrickwuke@163.com>
2022-04-28 20:53:19 +0800

    update latest blog

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-echo "commit and generate site....."
-git add .
-git commit -m "update blog sources"
-hugo --minify --gc
-# echo "submit in gh-pages"
-# git checkout gh-pages
-# cp -rf public/* .
-# rm -rf docs/
+# echo "commit and generate site....."
 # git add .
-# git commit -m "update latest blog"
-# git push 
+# git commit -m "update blog sources"
+# hugo --minify --gc
+echo "submit in gh-pages"
+git checkout gh-pages
+cp -rf public/* .
+rm -rf public/
+git add .
+git commit -m "update latest blog"
+git push 
 
 # echo "update main branch ...."
 # git push
